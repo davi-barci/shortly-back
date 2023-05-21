27f71a11c8cb1cb2a23040a7a272bc18f9762e8a
Davi Barci <davibarci0208@gmail.com>
2023-05-21 01:40:15 -0300

    feat: add sign in function

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -22,4 +22,25 @@ export async function signUp (req, res) {
     }
 };
 
+export async function signIn (req, res) {
+    const { email, password } = req.body;
+
+    try{
+        const user = await db.query(`SELECT * FROM users WHERE email=$1`, [email]);
+
+        if (!user.rowCount) return res.status(401).send("E-mail não cadastrado");
+        if (!bcrypt.compareSync(password, user.rows[0].password)) return res.status(401).send("Senha incorreta");
+          
+        const token = uuid();
+
+        await db.query(
+          `INSERT INTO sessions ("userId",token)
+                VALUES ($1,$2)`,
+          [user.rows[0].id, token]
+        );
+        res.status(200).send({ token: token });
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
 
